67ddfc171a598bcbb14377a6f943b6f1bb4ee33a
Christopher Evan <christopher.evan0603@gmail.com>
2022-12-27 00:18:58 +0700

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@
 
 import streamlit as st
 import os
-from tensorflow.keras.models import load_model
+from keras.models import load_model
 from PIL import Image
 import numpy as np
 from skimage import transform
